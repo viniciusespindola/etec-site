82f9673bea333dd3b62b50055fac0a7e8c51bd22
leo220401 <41969150+leo220401@users.noreply.github.com>
2018-10-23 14:08:15 -0300

    Fonts,Header e Footer
    ta ai

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+	<div id="copyright" class="container">
+		<p>&copy; Copyright © 2018 Etec Praia Grande | Photos by <a href="http://fotogrph.com/">Vinicius</a> | Design by <a href="http://templated.co" rel="nofollow">HENRIQUE</a>.</p>
+	</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!--
+Name       : RedMarket 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140101
+
+-->
+<html >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1><a href="#">ETEC PRAIA GRANDE</a></h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="home"><a href="index.php" accesskey="1" title="">HOME</a></li>
+				<li class="cursos"><a href="cursos.php" accesskey="2" title="">CURSOS</a></li>
+				<li class="direcao"><a href="direcao.php" accesskey="3" title="">DIREÇÃO</a></li>
+				<li class="sobre_nos"><a href="sobre.php" accesskey="4" title="">SOBRE NÓS</a></li>
+				<li class="contato"><a href="contato.php" accesskey="5" title="">CONTATE-NOS</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="banner" class="container"></div>
+</div>
