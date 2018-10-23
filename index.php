7ed0642019e9ca3712e3e4a7f547a2892e12c9bf
Vinicius Marinho <vm_espindola@hotmail.com>
2018-10-23 12:03:00 +0000

    pagina inicial criada

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php //get_header(); ?>
+<?php include_once 'header.php'; ?>
+
+<style type="text/css">
+	#menu li.home a, #menu li.home span{
+		background: #CC0000;
+	}
+</style>
+
+<div id="wrapper">
+	<div id="staff" class="container">
+		<div class="title">
+			<h2>ETEC EXTENSÃO BALNEÁRIO MARACANÃ</h2>
+			<span>Av. Dr. Roberto de Almeida Vinhas, 10.119 - Balneário Maracanã - Caixa Postal 11702240</span> </div>
+		<div class="boxA"><div class="zoom"><img src="images/foto1.jpg" width="300" height="450" alt="" /></div></div>
+		<div class="boxB"><div class="zoom"><img src="images/calendrio.png" width="300" height="450" alt="" /></div></div>
+		<div class="boxC"><div class="zoom"><img src="images/foto3.jpg" width="300" height="450" alt="" /></div></div>
+	</div>
+	<div id="page" class="container">
+		<div class="boxA">
+			<h2>Fusce ultrices<br />
+				<span>fringilla metus</span></h2>
+			<p>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum.</p>
+			<ul class="style4">
+				<li class="first"><a href="#">Consectetuer adipiscing elit</a></li>
+				<li><a href="#">Metus aliquam pellentesque</a></li>
+				<li><a href="#">Proin gravida orci porttitor</a></li>
+			</ul>
+		</div>
+		<div class="boxB">
+			<h2>Eventos<br />
+				<span>Mês de dezembro:</span></h2>
+			<ul class="style3">
+				<li class="first">
+					<p class="date"><a href="#">Dez<b>02</b></a></p>
+					<h3>Apresentação de TCC</h3>
+					<p><a href="#">Acontecerá apresentação de TCC para os 3º ANOS.</a></p>
+				</li>
+				<li>
+					<p class="date"><a href="#">Dez<b>18</b></a></p>
+					<h3>Ferias escolares</h3>
+					<p><a href="#">Começa o periodo de ferias escolares.</a></p>
+				</li>
+			</ul>
+		</div>
+		<div class="boxC">
+			<h2>Etiam posuere<br />
+				<span>augue sit amet</span></h2>
+			<ul class="style4">
+				<li class="first"><a href="#">Consectetuer adipiscing elit</a></li>
+				<li><a href="#">Metus aliquam pellentesque</a></li>
+				<li><a href="#">Suspendisse iaculis mauris</a></li>
+				<li><a href="#">Urnanet non molestie semper</a></li>
+				<li><a href="#">Suspendisse iaculis mauris</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="welcome" class="container">
+		<div class="title">
+			<h3>YouTuber dá dicas para aproveitar os estudos e se preparar para o Vestibulinho das Etecs</h3>
+		</div>
+		<p>Com playlists de vídeos em canal no <strong><a href="https://www.youtube.com/watch?v=kLdO1sJnJzc">YouTube</a></strong>, a ex-aluna do curso técnico de Farmácia da Escola Técnica Estadual de Praia Grande, Lillian Cruz, explica maneiras de otimizar os estudos para o exame, que será aplicado no próximo dia 21 de janeiro</p>
+
+		<div class="zoom">
+			<a href="https://www.cps.sp.gov.br/youtuber-da-dicas-para-aproveitar-os-estudos-ao-maximo-e-se-preparar-para-o-vestibulinho-das-etecs/"><img src="images/lillianfoto-580x420.jpg" width="580" height="420" alt="" /></a>
+		</div>
+	</div>
+</div>
+
+<?php include_once 'footer.php'; ?>
+<?php //get_footer(); ?>
