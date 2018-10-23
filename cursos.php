6fadd5a340d3c92f8825e76d259e05ad5ff35a32
Henrique Pereira <henrique4ever@hotmail.com.br>
2018-10-23 17:11:09 +0000

    Adicionado paginas da barra de tarefa

---
+++ cursos.php
@@ -0,0 +1,56 @@
+<?php //get_header(); ?>
+<?php include_once 'header.php'; ?>
+
+<style type="text/css">
+	#menu li.cursos a, #menu li.cursos span{
+		background: #CC0000;
+	}
+</style>
+
+<div></div>
+<div id="wrapper">
+	<div id="page" class="container">
+		<h2 class="mt-5">Cursos oferecidos</h2>
+
+		<ul class="listas style4">
+			<li class="first">
+				<strong>Administração - Integrado ao Ensino Médio</strong>
+				<p>Período: Integral - 40 vagas</p>
+			</li>
+
+			<li>
+				<strong>Contabilidade</strong>
+				<p>Período: Noite - 40 vagas</p>			
+			</li>
+
+			<li>
+				<strong>Contabilidade</strong>
+				<p>Período: Noite - 40 vagas</br>
+				<b>Obs: </b><span class="text-muted">Para o Curso de Técnico em Contabilidade a certificação, desde 02/06/2015, não prevê a obtenção de registro de Conselho da categoria da classe, conforme o disposto na Lei nº 12.249/10, que alterou o Decreto-Lei nº 9295, de 27 de maio de 1946, artigo 12, parágrafo segundo.</span></p>		
+			</li>
+
+			<li>
+				<strong>Informática para Internet</strong>
+				<p>Período: Noite - 40 vagas</p>		
+			</li>
+
+			<li>
+				<strong>Informática para Internet - Integrado ao Ensino Médio</strong>
+				<p>Período: Integral - 40 vagas</p>
+			</li>
+
+			<li>
+				<strong>Marketing - Integrado ao Ensino Médio</strong>
+				<p>Período: Integral - 40 vagas</p>
+			</li>
+
+			<li>
+				<strong>Segurança do Trabalho - Integrado ao Ensino Médio</strong>
+				<p>Período: Integral - 40 vagas</p>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<?php include_once 'footer.php'; ?>
+<?php //get_footer(); ?>
---
+++ direcao.php
@@ -0,0 +1,31 @@
+<?php //get_header(); ?>
+<?php include_once 'header.php'; ?>
+
+<style type="text/css">
+	#menu li.direcao a, #menu li.direcao span{
+		background: #CC0000;
+	}
+</style>
+
+<div id="wrapper">
+	<div id="page" class="container">
+
+	
+		<img src="images/direcao-300x200.jpg" width="600" height="400" class="img my-5">
+	
+
+		<ul class="text-muted font-comic top">
+			<li>Profa. Me. Fabiana Golz Ribeiro Pereira</li>
+			<li>Mestre em Educação</li>
+			<li>Pós-graduada em Gestão Estratégica Escolar</li>
+			<li>Licenciada em Pedagogia</li>
+			<li>Licenciada em Informática</li>
+			<li>Tecnóloga em Processamento de Dados</li>
+			<li>Atua como diretora de ETEC desde maio de 2006.</li>
+			<li>Assumiu a direção da Etec Aristóteles Ferreira em Janeiro de 2015.</li>
+		</ul>
+	</div>
+</div>
+
+<?php include_once 'footer.php'; ?>
+<?php //get_footer(); ?>
