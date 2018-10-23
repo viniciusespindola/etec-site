749eb8a997c51b4924a5cce19932b47934b669ac
Victoria Aquino <victoria.aquino015@gmail.com>
2018-10-23 17:17:34 +0000

    comitando sobre, contato e imagens

---
+++ contato.php
@@ -0,0 +1,31 @@
+<?php //get_header(); ?>
+<?php include_once 'header.php'; ?>
+
+<style type="text/css">
+	#menu li.contato a, #menu li.contato span{
+		background: #CC0000;
+	}
+</style>
+
+<div id="wrapper">
+	<div id="page" class="container">
+		<h2 class="mt-5 ">Etec de Praia Grande - Extensão Balneário Maracanã</h2>
+		
+		<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d2970.506774250256!2d-46.50688192851279!3d-24.03214575641077!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1f79fee2cc37%3A0x6ec2fc95ba763c29!2sEtec+de+Praia+Grande!5e1!3m2!1spt-BR!2sbr!4v1540262740592" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		
+		<br><br><p><i class="fas fa-map-marker-alt text-red"></i> Av. Dr. Roberto de Almeida Vinhas, 10.119 - Balneário Maracanã - <wbr>Caixa Postal 11702240
+		CEP 11705-740 - Praia Grande/SP</p>
+		<ul class="style3 top">
+			
+			<li class="first"><i class="fas fa-phone text-red"></i> Telefone: (13) 3471-2395</li>
+			<li><i class="far fa-envelope text-red"></i> E-mail: e153dir@cps.sp.gov.br</li>
+			<li><i class="far fa-envelope text-red"></i> E-mail secretaria.etecpg@gmail.com</li>
+			<li><i class="fas fa-link text-red"></i> Site: www.fatecpg.com.br</li>
+		</ul>
+
+
+	</div>
+</div>
+
+<?php include_once 'footer.php'; ?>
+<?php //get_footer(); ?>
---
+++ sobre.php
@@ -0,0 +1,43 @@
+<?php //get_header(); ?>
+<?php include_once 'header.php'; ?>
+
+<style type="text/css">
+	#menu li.sobre_nos a, #menu li.sobre_nos span{
+		background: #CC0000;
+	}
+	.staff2 {
+		font-size: 20px;
+	
+	}
+	
+</style>
+
+<div id="wrapper">
+	<div id="page" class="container">
+		<div class="staff2">
+				<h1 class="mt-5 text-center">SOBRE NÓS</h1> 
+			
+				<h3 class="my-5 text-red">"MISSÃO DA ETEC DE PRAIA GRANDE"</h3>
+			
+				<SPAN>Oferecer a Educação Basica e Educação profissional aos jovens e adultos do municipio de Praia Grande e região para que sejam felizes, produtivos, capazes de melhorar sua propría vida e de todos os espaços em que convivem.</SPAN>
+			
+				<h3 class="my-5 text-red">"VISÃO DA ETEC DE PRAIA GRANDE"</h3>
+				
+				<SPAN>Ser uma Etec reconhecida na região pelos padroes de qualidade no oferecimento da Educação basica (Ensino Médio) e edicação Profissional, articulada com o mercado de trabalho para diminuir a distancia entre a escola e a empregabilidade.</SPAN>
+				
+				<h3 class="my-5 text-red">"VALORES DA ETEC DE PRAIA GRANDE"</h3>
+				
+				<ul class="style4 mb-5">
+					<LI>UNIÃO</LI>
+					<li>COOPERAÇÃO</li>
+					<li>PERTINENCIA</li>
+					<li>SOLIDARIEDADE</li>
+					<li>DIZER A VERDADE</li>
+					<li>ACEITAÇÃO DO OUTRO</li>
+				</ul>
+			</div>
+	</div>
+</div>
+
+<?php include_once 'footer.php'; ?>
+<?php //get_footer(); ?>
